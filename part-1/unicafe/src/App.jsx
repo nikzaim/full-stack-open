e0b15b6c773265c2bd404f6f50fa3916e861c63a
nikza <156214902+nikzaim@users.noreply.github.com>
2024-07-01 15:30:55 +0800

    add 1.11

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -58,12 +58,16 @@ const Statistics = (props) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.all} />
-      <StatisticLine text="average" value={props.total / props.all} />
-      <StatisticLine text="positive" value={props.positive} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.all} />
+          <StatisticLine text="average" value={props.total / props.all} />
+          <StatisticLine text="positive" value={props.positive} />
+        </tbody>
+      </table>
     </>
   );
 };
@@ -71,8 +75,9 @@ const Statistics = (props) => {
 const StatisticLine = (props) => {
   const { text, value } = props;
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
